dbcca4d66d0090d69dc85519c18e0895ab97203d
Tiberiu Ichim <tiberiu.ichim@gmail.com>
2019-11-04 22:32:13 +0200

    Fix repo name in setup script

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-git clone https://github.com/tiberiuichim/fullstack_skeleton .skel
+git clone https://github.com/tiberiuichim/fullstack-skeleton .skel
 cp -r .skel/* .
 mv README.md.new README.md
 rm -rf .skel
