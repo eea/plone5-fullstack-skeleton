4bda77bc60b9139f8dcab1ba3107668ba17161a0
Tiberiu Ichim <tiberiu.ichim@gmail.com>
2019-11-08 13:12:47 +0200

    Move readme to tpl dir

--- setup.sh
+++ setup.sh
@@ -8,13 +8,15 @@ if [ -d frontend ]; then
   rm -rf .skel/_frontend
 fi;
 
-cp -rna .skel/* .
+# cp -rna .skel/* .
+cp -rna .skel/backend .
+cp -rna .skel/scripts .
 
 if [ ! -f .env ]; then
   mv .skel/tpl/env .env
 fi
 
 if [ ! -f README.md ]; then
-  mv README.md.new README.md
+  mv .skel/tpl/README.md.tpl README.md
 fi
 rm -rf .skel
